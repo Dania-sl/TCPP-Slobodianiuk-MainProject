7e201f4138220cbc776323f6fc1c0be6b34516fd
Даня Слободянюк <60923430+Dania-sl@users.noreply.github.com>
2021-03-14 20:32:00 +0200

    Змінено з браузера

--- TCPP-Slobodianiuk-MainProject/Program.cs
+++ TCPP-Slobodianiuk-MainProject/Program.cs
@@ -7,7 +7,7 @@ namespace TCPP_Slobodianiuk_MainProject
         static void Main(string[] args)
         {
             Console.Write("Hello Donnu!");
-            Console.Write("Змінено з IDE!");
+            Console.Write("Змінено з браузера!");
         }
     }
 }
